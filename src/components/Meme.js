ff8c19bb472fd471a8a4b8e811307080d621c52f
Aditya Pansari <iadityapansari@gmail.com>
2022-02-08 10:30:28 +0530

    render random image on btn click adds local memes data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,31 @@
+import React from "react";
+import MemesData from "../memesData";
+
 export default function Meme() {
+  const [memeImg, setMemeImg] = React.useState("");
+
+  function onSumit() {
+    let index = Math.floor(Math.random() * 100);
+    setMemeImg(MemesData.data.memes[index].url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <div className="form--input">
           <input type="text" name="top" placeholder="top text" className="" />
           <input type="text" name="bottom" placeholder="bottom text" />
         </div>
         <div className="form--submit">
-          <input type="submit" name="submit" value="Get a new meme image 🖼" />
+          <input
+            onClick={onSumit}
+            type="button"
+            name="submit"
+            value="Get a new meme image 🖼"
+          />
         </div>
-      </form>
+        <div>{!!memeImg && <img className="meme--image" src={memeImg} />}</div>
+      </div>
     </main>
   );
 }
